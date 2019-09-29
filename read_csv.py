9bffe47dbbd070a0f78c70062d9c1a7401b91045
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2019-09-29 01:50:40 -0300

    add pk column, fix print putput

--- read_csv.py
+++ read_csv.py
@@ -4,24 +4,33 @@ import config
 
 encodingVar = "cp1252"
 filePath = '../microdados_censo_superior_2016/DADOS/DM_ALUNO.CSV'
-validColumns = [ 0, 2, 6, 9, 11, 13, 28, 30, 35, 39, 55, *range(57, 75), 105, 107 ]
+validColumns = [ 0, 2, 6, 9, 11, 13, 25, 28, 30, 35, 39, 55, *range(57, 75), 105, 107 ]
 batchSize = 100000
+
 engine = None
 df = None
+columnNames = []
 
 def ReadRows(i):
-	dataFrame = pd.read_csv(filePath, sep='|', header = None, encoding=encodingVar, skiprows=(i*batchSize), nrows=batchSize, usecols=validColumns)
+    skip = i * batchSize + 1
 
-	return dataFrame
+    dataFrame = pd.read_csv(filePath, sep='|', header = None, encoding=encodingVar, skiprows=skip, nrows=batchSize, usecols=validColumns, names=columnNames)
 
-def ReadHeaders():
-	headers = pd.read_csv(filePath, sep='|', header = 0, encoding=encodingVar, nrows=1, usecols=validColumns)
-	return headers
+    return dataFrame
+
+def CreateTable(engine):
+    df = pd.read_csv(filePath, sep='|', header = 0, encoding=encodingVar, nrows=0, usecols=validColumns)
+    df.to_sql(name='aluno', con=engine, if_exists='replace', index=False, index_label="CO_ALUNO_CURSO")
+    return df.columns
 
 def CreateConnection():
-	engine=create_engine("mysql+pymysql://" + config.username + ":" + config.password + "@localhost/microdados2016")
-	return engine
+    engine=create_engine("mysql+pymysql://" + config.username + ":" + config.password + "@localhost/microdados2016")
+    return engine
 
 engine = CreateConnection()
-df = ReadHeaders()
-df.to_sql(name='aluno', con=engine, if_exists='append')
+columnNames = CreateTable(engine)
+
+for i in range(1):
+    df = ReadRows(i)
+    df.to_sql(name='aluno', con=engine, if_exists='append', index=False, index_label="CO_ALUNO_CURSO")
+    print("inserted ", i + 1 * batchSize)
