5cbdee95ff10f5c8027ad76c15df088f557f46c1
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2019-10-04 09:21:36 -0300

    script preencher colunas co_ingresso e co_reserva

--- read_csv.py
+++ read_csv.py
@@ -34,12 +34,18 @@ def ReadRows(i, columns):
     skip = i * batchSize + 1
 
     dataFrame = pd.read_csv(filePath, sep='|', header = None, encoding=encodingVar, skiprows=skip, nrows=batchSize, usecols=validColumns, names=columns)
-    # dfFiltered = dataFrame[(dataFrame.CO_OCDE_AREA_GERAL == 4) & (dataFrame.CO_OCDE_AREA_ESPECIFICA > 80) & (dataFrame.CO_OCDE_AREA_ESPECIFICA < 83) & (dataFrame.CO_MODALIDADE_ENSINO == 1)]
+    dataFrame['CO_INGRESSO'] = ''
+    dataFrame['CO_RESERVA'] = ''
     dfFiltered = dataFrame[(dataFrame.CO_OCDE_AREA_DETALHADA > 480) & (dataFrame.CO_OCDE_AREA_DETALHADA < 484) & (dataFrame.CO_MODALIDADE_ENSINO == 1)]
+    for index, row in dfFiltered.iterrows():
+        dfFiltered.CO_INGRESSO[index] = ParseCOIngresso(row)
+        dfFiltered.CO_RESERVA[index] = ParseCOReserva(row)
     return dfFiltered
 
 def CreateTable(engine):
     df = pd.read_csv(filePath, sep='|', header = 0, encoding=encodingVar, nrows=0, usecols=validColumns)
+    df['CO_INGRESSO'] = ''
+    df['CO_RESERVA'] = ''
     df.to_sql(name=tableName, con=engine, if_exists='replace', index=False, index_label=indexLabel)
     return df.columns
 
@@ -47,11 +53,34 @@ def CreateConnection():
     engine=create_engine("mysql+pymysql://" + config.username + ":" + config.password + "@localhost/microdados2016")
     return engine
 
+def ParseCOIngresso(row):
+    if (row.IN_ING_VESTIBULAR == 1): return 'V'
+    if (row.IN_ING_ENEM == 1): return 'E'
+    if (row.IN_ING_AVALIACAO_SERIADA == 1): return 'AS'
+    if (row.IN_ING_SELECAO_SIMPLIFICADA == 1): return 'SS'
+    if (row.IN_ING_SELECAO_VAGA_REMANESC == 1): return 'VR'
+    if (row.IN_ING_SELECAO_VAGA_PROG_ESPEC == 1): return 'VP'
+    if (row.IN_ING_TRANSF_EXOFFICIO == 1): return 'TE'
+    if (row.IN_ING_DECISAO_JUDICIAL == 1): return 'DJ'
+    if (row.IN_ING_CONVENIO_PECG == 1): return 'CP'
+    return 'NA'
+
+def ParseCOReserva(row):
+    if (row.IN_RESERVA_VAGAS == 1):
+        if (row.IN_RESERVA_ETNICO == 1): return 'RE'
+        if (row.IN_RESERVA_DEFICIENCIA == 1): return 'RD'
+        if (row.IN_RESERVA_ENSINO_PUBLICO == 1): return 'EP'
+        if (row.IN_RESERVA_RENDA_FAMILIAR == 1): return 'RF'
+        if (row.IN_RESERVA_OUTRA == 1): return 'RO'
+        return 'NA'
+    else:
+        return 'NA'
+
 engine = CreateConnection()
 # Enable to create table
 columnNames = CreateTable(engine)
 
-for i in range(0,14):
+for i in range(0,12):
     df = ReadRows(i, tb_aluno_column_names.columnNames)
     df.to_sql(name=tableName, con=engine, if_exists='append', index=False, index_label=indexLabel)
     print("inserted ", (i + 1) * batchSize)
